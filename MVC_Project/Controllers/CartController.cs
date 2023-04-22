4fb9a2f841733bc30c9d0866545609c86deeefde
dinhtuannam <namdinhtuan5@gmail.com>
2023-04-22 17:07:57 +0700

    User Template

---
+++ MVC_Project/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Controllers
+{
+	public class CartController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
+}
---
+++ MVC_Project/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Controllers
+{
+	public class RegisterController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
