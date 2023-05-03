e20fe22a12dfc9299285f00abd01736d073e0983
dinhtuannam <namdinhtuan5@gmail.com>
2023-05-03 20:44:40 +0700

    Nhận mã giảm giá trong giỏ hàng

--- MVC_Project/Controllers/DiscountController.cs
+++ MVC_Project/Controllers/DiscountController.cs
@@ -1,16 +1,21 @@
-﻿using Entity.Enums;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Entity.Enums;
 using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Helper;
 using MVC_Project.Models;
 using Services;
+using Services.Implementation;
 
 namespace MVC_Project.Controllers
 {
     public class DiscountController : Controller
     {
         private IDiscounts _discountService;
-        public DiscountController(IDiscounts discountService)
+        public INotyfService _notifyService { get; }
+        public DiscountController(IDiscounts discountService, INotyfService notifyService)
         {
             _discountService = discountService;
+            _notifyService = notifyService;
         }
 
         public IActionResult Index()
@@ -34,6 +39,64 @@ namespace MVC_Project.Controllers
                 Status = model.Status
             };
             return Json(discountModel);
+        }
+
+        public Cart_Index_VM Carts
+        {
+            get
+            {
+                var data = HttpContext.Session.Get<Cart_Index_VM>("GioHang");
+                if (data == null)
+                {
+                    data = new Cart_Index_VM();
+                }
+                return data;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ApplyDiscount(string? name)
+        {
+            if(name == null)
+                return View();
+            var myCart = Carts;
+            var model = _discountService.GetByName(name);
+            if (model == null)
+            {
+                _notifyService.Information("Discount not found");
+                return Json(new
+                {
+                    status = "notfound",
+                });
+            }
+            var discountModel = new Discount_VM
+            {
+                DiscountId = model.DiscountId,
+                DiscountName = model.DiscountName,
+                DiscountPrice = model.DiscountPrice,
+                Description = model.Description,
+                Status = model.Status
+            };
+            myCart.DiscountPrice = discountModel.DiscountPrice;
+            HttpContext.Session.Set("GioHang", myCart);
+            _notifyService.Success("Apply discount successfully");
+            return Json(new
+            {   
+                status = "success",
+            });
+        }
+
+        [HttpGet]
+        public IActionResult CancelDiscount()
+        {
+            var myCart = Carts;
+            myCart.DiscountPrice = 0;
+            HttpContext.Session.Set("GioHang", myCart);
+            _notifyService.Success("Cancel discount successfully");
+            return Json(new
+            {
+                status = "success",
+            });
         }
     }
 }
--- MVC_Project/Models/Cart_Index_VM.cs
+++ MVC_Project/Models/Cart_Index_VM.cs
@@ -7,10 +7,17 @@
 			CartItems = new List<Cart_item>();
 		}
 		public List<Cart_item> CartItems { get; set; }
-		public double CartTotal
+		public double CartSubTotal
 		{
-			get { return CartItems.Sum(item => item.Total); }
+			get { return CartItems.Sum(item => item.Total) ; }
 		}
-		public double DiscountPrice { get; set; }
+        public double CartTotal
+        {
+            get { return CartSubTotal + DiscountPrice + 5; }
+        }
+        public double DiscountPrice { get; set; }
+		public string Fullname { get; set; }
+		public string Address { get; set; }
+		public string PhoneNumber { get; set; }
 	}
 }
